ad9140171851eece2b9a1d511f68179520d88260
void2610 <izumisan2610@gmail.com>
2024-05-20 00:22:33 +0900

    delete old camara

---
+++ Assets/Scprits/GameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using Unity.Netcode;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private List<float> playerScores = new List<float>();
+    private int playerCount = 0;
+    private bool onGame = false;
+    private int itIndex;
+
+    public void StartGame()
+    {
+        playerCount = NetworkManager.Singleton.ConnectedClientsList.Count;
+        playerScores.Clear();
+        for (int i = 0; i < playerCount; i++)
+        {
+            playerScores.Add(0);
+        }
+        itIndex = Random.Range(0, playerCount);
+    }
+
+    public void ChangeIt(int index)
+    {
+        itIndex = index;
+    }
+
+    private void Start()
+    {
+        //3秒後にゲームを開始
+        Invoke("StartGame", 3);
+    }
+
+    public void Update()
+    {
+        if (onGame)
+        {
+            playerScores[itIndex] += Time.deltaTime;
+        }
+    }
+}
