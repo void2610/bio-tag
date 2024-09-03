6274bf9571ca626e021dd2ece1cb02518cc2832b
void2610 <izumisan2610@gmail.com>
2024-09-03 15:46:22 +0900

    npc name tag

--- Assets/Scprits/Player/NPC.cs
+++ Assets/Scprits/Player/NPC.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    [SerializeField]
+    protected GameObject playerNameUIPrefab;
     public int index = -1;
     void Start()
     {
-
+        var canvas = GameObject.Find("WorldSpaceCanvas");
+        var ui = Instantiate(playerNameUIPrefab, canvas.transform);
+        ui.GetComponent<PlayerNameUI>().SetTargetPlayer(this.gameObject, "NPC" + index);
     }
 
     // Update is called once per frame
