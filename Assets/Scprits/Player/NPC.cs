8cb526d5e29952b21e696494da5039d0a34f18e7
void2610 <izumisan2610@gmail.com>
2024-09-04 14:11:19 +0900

    Update NPC.cs

--- Assets/Scprits/Player/NPC.cs
+++ Assets/Scprits/Player/NPC.cs
@@ -41,7 +41,6 @@ public class NPC : MonoBehaviour
         agent.speed = moveSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (target != null && GameManagerBase.instance.gameState == 1)
@@ -54,12 +53,12 @@ public class NPC : MonoBehaviour
             {
                 agent.SetDestination(target.position);
             }
-            animator.SetFloat("Speed", agent.velocity.magnitude); // アニメーションの速度を設定
+            animator.SetFloat("Speed", agent.velocity.magnitude);
             animator.SetFloat("MotionSpeed", 1);
         }
         else
         {
-            animator.SetFloat("Speed", 0); // 停止時のアニメーション
+            animator.SetFloat("Speed", 0);
             animator.SetFloat("MotionSpeed", 0);
         }
 
@@ -74,19 +73,13 @@ public class NPC : MonoBehaviour
         isJumping = true;
         animator.SetBool("Jump", true);
         animator.SetBool("Grounded", false);
-        float originalSpeed = agent.speed;
 
-        // エージェントの自動位置更新を無効化
         agent.updatePosition = false;
 
-        // オフメッシュリンクのデータを取得
-        OffMeshLinkData linkData = agent.currentOffMeshLinkData;
-
         Vector3 startPos = agent.transform.position;
-        Vector3 endPos = linkData.endPos + Vector3.up * agent.baseOffset;
+        Vector3 endPos = agent.currentOffMeshLinkData.endPos + Vector3.up * agent.baseOffset;
 
-        float distance = Vector3.Distance(startPos, endPos);
-        float duration = distance / (originalSpeed * 0.8f); // 移動にかかる時間を計算
+        float duration = Vector3.Distance(startPos, endPos) / (moveSpeed * 0.8f);
 
         float time = 0;
 
@@ -98,21 +91,19 @@ public class NPC : MonoBehaviour
             yield return null;
         }
 
-        // 最後にエージェントを目的地に正確に配置
         agent.transform.position = endPos;
-
-        // オフメッシュリンクの移動を完了
         agent.CompleteOffMeshLink();
 
-        // NavMeshAgentの位置を強制的に再同期
+
         agent.Warp(endPos);
         animator.SetBool("Jump", false);
         animator.SetBool("Grounded", true);
 
-        yield return new WaitForSeconds(0.5f);
+        // ジャンプ後の待機時間
+        agent.speed = 0;
+        yield return new WaitForSeconds(0.3f);
 
-        // 元のスピードに戻し、エージェントの位置更新を再度有効化
-        agent.speed = originalSpeed;
+        agent.speed = moveSpeed;
         agent.updatePosition = true;
         isJumping = false;
     }
