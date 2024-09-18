b1998a124c506936c8aa59cdb8f1ae89d223e041
void2610 <izumisan2610@gmail.com>
2024-09-18 16:12:58 +0900

    player trail

--- Assets/Scprits/Player/NPC.cs
+++ Assets/Scprits/Player/NPC.cs
@@ -19,7 +19,6 @@ public class NPC : MonoBehaviour
     private NavMeshAgent agent => this.GetComponent<NavMeshAgent>();
     private int jumpAreaType = 3;
     private bool isMovable = true;
-    private Vector3 lastFootPrintPosition = Vector3.zero;
 
     public void Wait(float time)
     {
@@ -64,12 +63,6 @@ public class NPC : MonoBehaviour
             }
             animator.SetFloat("Speed", agent.velocity.magnitude);
             animator.SetFloat("MotionSpeed", 1);
-
-            if (Vector3.Distance(lastFootPrintPosition, transform.position) > 1.0f)
-            {
-                FootPrintManager.instance.CreateFootPrint(transform.position, transform.rotation, index);
-                lastFootPrintPosition = transform.position;
-            }
         }
         else
         {
