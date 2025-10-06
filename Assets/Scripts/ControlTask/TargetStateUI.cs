cddbaa95899fa0ce77d5fd0015b3ca1f0edc0444
void2610 <izumisan2610@gmail.com>
2025-10-06 09:02:42 +0900

    fix task

--- Assets/Scripts/ControlTask/TargetStateUI.cs
+++ Assets/Scripts/ControlTask/TargetStateUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace ControlTask
 {
+    [RequireComponent(typeof(TextMeshProUGUI))]
     public class TargetStateUI : MonoBehaviour
     {
         private TextMeshProUGUI _text;
@@ -12,9 +13,24 @@ namespace ControlTask
             _text = this.GetComponent<TextMeshProUGUI>();
             ControlTaskManager.Instance.TargetState.Subscribe((state) =>
             {
-                _text.text = state.ToString();
+                _text.text = GetJapaneseStateName(state);
                 _text.color = state == ControlState.Excited ? Color.red : Color.white;
             }).AddTo(this);
+        }
+    
+        private string GetJapaneseStateName(ControlState state)
+        {
+            return state switch
+            {
+                ControlState.Calibration => "キャリブレーション中",
+                ControlState.GoalPresentation => "目標表示中",
+                ControlState.Preparation => "準備中",
+                ControlState.Excited => "目標: 興奮",
+                ControlState.Calmed => "目標: 冷静",
+                ControlState.Feedback => "結果表示中",
+                ControlState.Rest => "休憩中",
+                _ => ""
+            };
         }
     }
 }
